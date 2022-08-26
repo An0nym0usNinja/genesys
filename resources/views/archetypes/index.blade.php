e3b9f595f800e4447f4c33d703849a61c87e5513
Adriaan Mouton <arouxmouton@gmail.com>
2022-08-26 17:02:30 +0200

    added buttons for index page

--- resources/views/archetypes/index.blade.php
+++ resources/views/archetypes/index.blade.php
@@ -2,21 +2,36 @@
 
 @section('content')
 
-    <table class="table table-dark table-hover table-sm index-table">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($archetypes as $archetype)
+    <section class="align-right">
+        <a href="" class="btn-action btn-action-md">
+            <img src="{{asset('icons/add.png')}}" alt="">
+        </a>
+    </section>
+    
+    <section>
+        <table class="table table-dark table-hover table-sm index-table">
+            <thead>
                 <tr>
-                    <td>{{$archetype->name}}</td>
-                    <td class="text-neon-blue text-center">CRB</td>
+                    <th>Name</th>
+                    <th>Actions</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($archetypes as $archetype)
+                    <tr>
+                        <td>{{$archetype->name}}</td>
+                        <td class="text-center">
+                            <a href="" class="btn-action btn-action-sm">
+                                <img src="{{asset('icons/edit.png')}}" alt="">
+                            </a>
+                            <a href="" class="btn-action btn-action-sm">
+                                <img src="{{asset('icons/delete.png')}}" alt="">
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </section>
 
 @endsection
