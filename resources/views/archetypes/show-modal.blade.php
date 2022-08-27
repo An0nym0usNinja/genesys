6f0e63ea914e522be078a2d548b60b714a54fc3d
Adriaan Mouton <arouxmouton@gmail.com>
2022-08-27 19:45:25 +0200

    added archetypes show modal (uncomplete)

---
+++ resources/views/archetypes/show-modal.blade.php
@@ -0,0 +1,31 @@
+<div class="modal fade" id="itemShowModal{{ $item->id }}" tabindex="-1" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title text-neon-blue" id="exampleModalLabel">{{$item->name}}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <nav>
+                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                        <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab"
+                            data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home"
+                            aria-selected="true">Description</button>
+                        {{-- <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile"
+                            type="button" role="tab" aria-controls="nav-profile"
+                            aria-selected="false">Profile</button> --}}
+                    </div>
+                </nav>
+                <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab"
+                        tabindex="0">
+                        <p class="item-description">{{$item->description}}</p>
+                    </div>
+                    {{-- <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab"
+                        tabindex="0">content 2</div> --}}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
